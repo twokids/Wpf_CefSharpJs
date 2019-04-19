b3e042a6f435f986df7249f4b67b88919f105020
nanshan03 <734463238@qq.com>
2019-04-19 14:19:22 +0800

    feat:搭建wpf项目，cefsharp和js交互demo，js调用并传参给.net代码，.net返回结果

---
+++ DotNetMessage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CefSharpJs
+{
+
+    public class DotNetMessage
+    {
+        MainWindow _mainWindow;
+        private static List<Animal> animals = new List<Animal>();
+        public DotNetMessage(MainWindow mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+        public string showwindow(string message)
+        {
+            return "你好，" + message;
+        }
+
+        public List<Animal> getanimals()
+        {
+            return animals;
+        }
+
+        public List<Animal> addanimal(string json)
+        {
+            animals.Add(new Animal() { Name = json });
+            return animals;
+        }
+    }
+
+    public class Animal
+    {
+        public string Name { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CefSharpJs
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeWebView();
+
+
+        }
+
+        /// <summary>
+        /// 初始化浏览器控件
+        /// </summary>
+        private void InitializeWebView()
+        {
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+            webView.RegisterAsyncJsObject("dotNetMessage", new DotNetMessage(this));
+            //bin 目录中
+            string path = AppDomain.CurrentDomain.BaseDirectory + @"templates\index.html";
+            webView.Address = path;
+            webView.FrameLoadEnd += OnBrowserFrameLoadEnd;        
+        }
+
+        private void OnBrowserFrameLoadEnd(object sender, FrameLoadEndEventArgs args)
+        {
+            if (args.Frame.IsMain)
+            {
+                webView.ExecuteScriptAsync("indexJs.jsMethod('你好')");
+            }
+        }
+
+    }
+}
